ca20a1f975d31853a21a4c3b9aaef6835be363aa
franklion <franklion1227@gmail.com>
2018-09-03 16:32:52 +0800

    [ update init ]

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <a href="#" @click.prevent="signout">登出</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  methods: {
+    signout() {
+      const api = `${process.env.VUE_APP_APIPATH}/logout`
+      this.$http.post(api).then((response) => {
+        console.log(response.data)
+
+        if (response.data.success) {
+          this.$router.push('/login')
+        }
+      })
+    },
+  },
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+import router from './router/index'
+
+Vue.config.productionTip = false
+
+
+axios.defaults.withCredentials = true
+
+Vue.use(VueAxios, axios)
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
+
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`
+    axios.post(api).then((response) => {
+      console.log(response.data)
+
+      if (response.data.success) {
+        next()
+      } else {
+        next({
+          path: '/login',
+        })
+      }
+    })
+  } else {
+    next()
+  }
+})
